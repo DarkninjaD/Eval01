ccec68b4c9f6c5553882f36d9fbf75ceb9217bd6
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2023-11-16 22:59:44 -0600

    clean up

--- app/src/main/kotlin/eval01/utils/Cui.kt
+++ app/src/main/kotlin/eval01/utils/Cui.kt
@@ -1,9 +1,4 @@
 package eval01.utils
-import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.options.prompt
-import java.util.LinkedList
-import java.util.Queue
 import kotlin.text.trim
 
 
